9382d04afd0419fb747185d1352316f74b8a3a72
HorikitanFutaba <votrungnam2013@gmail.com>
2021-08-26 09:05:00 +0700

    update bai 4

---
+++ js/config.js
@@ -0,0 +1,22 @@
+var mysql = require('mysql');
+
+var connect = mysql.createConnection({
+    host: "localhost",
+    user: 'root',
+    password: "",
+    database: "test"
+})
+
+function addUser(username, password, email, birthday){
+     connect.connect(function(err){
+          if (err) throw err;
+          let sql = "insert into users values('"+username+"', '" + password +"','" + email + "','" + birthday +"')";
+          connect.query(sql, (err, reulst) => {
+              if (err) throw err;
+              console.log(reulst)
+          });
+     })
+
+}
+
+module.exports.addUser = addUser;
