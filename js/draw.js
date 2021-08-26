82c721c7c511eb4ec3183201b9a76d097b875f06
HorikitanFutaba <votrungnam2013@gmail.com>
2021-08-26 17:32:23 +0700

    update bai 5

---
+++ js/draw.js
@@ -0,0 +1,90 @@
+function drawText(ctx, centerX, centerY, text, fontSize, center = 0, italic = 0) {
+  ctx.save();
+  ctx.beginPath();
+  ctx.moveTo(centerX, centerY);
+  ctx.fillStyle = "black";
+  if (italic == 1)
+    ctx.font = "italic " + fontSize + "px Arial";
+  else
+    ctx.font = fontSize + "px Arial";
+  if (center == 1)
+    ctx.textAlign = "center";
+  else
+    ctx.textAlign = "right";
+  ctx.fillText(text, centerX, centerY);
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
+  ctx.restore();
+}
+function drawVerText(ctx, centerX, centerY, text, fontSize) {
+  ctx.save();
+  ctx.font = "italic " + fontSize + "px Arial";
+  ctx.fillStyle = "black";
+  ctx.translate(centerX, centerY);
+  ctx.rotate(-0.5 * Math.PI);
+  ctx.fillText(text, 0, 0);
+  ctx.restore();
+}
+function drawLine(ctx, startX, startY, endX, endY) {
+  ctx.beginPath();
+  ctx.fillStyle = "red";
+  ctx.moveTo(startX, startY);
+  ctx.lineTo(endX, endY);
+  ctx.stroke();
+}
+function drawRect(ctx, startX, startY, endX, endY, color) {
+  ctx.beginPath();
+  ctx.fillStyle = color;
+  ctx.fillRect(startX, startY, endX, endY);
+  ctx.stroke();
+}
+function drawGraphCol(posX, posY, numY, numX, color, nameChart, nameProject, nameComment, yCount) {
+  const colGraph = document.getElementById("col__chart");
+  const ctx = colGraph.getContext("2d");
+  const xcont = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'];
+  //vẽ text tên biểu đồ
+  drawText(ctx, posX + numX * 50, posY - 50, nameChart, 20, 1);
+  //vẽ text ngang của biểu đồ
+  drawVerText(ctx, posX - 50, posY + numY * 50 / 1.5, nameComment, 20);
+  //Vẽ text tên dự án
+  drawText(ctx, posX + numX * 50, posY + numY * 50 + 60, nameProject, 20, 1, 1);
+  //Vẽ chú thích
+  if (numX > 9) {
+    if (numY <= 6) {
+      drawRect(ctx, posX + numX * 30, posY + numY * 57 + 60, 80, 30, color);
+      drawText(ctx, posX + numX * 30 + 230, posY + numY * 57 + 80, nameComment, 20, 1);
+    }
+    else {
+      drawRect(ctx, posX + numX * 30, posY + numY * 57 + 20, 80, 30, color);
+      drawText(ctx, posX + numX * 30 + 230, posY + numY * 57 + 40, nameComment, 20, 1);
+    }
+  }
+  else {
+    drawRect(ctx, posX + 50 + numX * 100, posY, 80, 30, color);
+    drawText(ctx, posX + 80 + numX * 100 + 180, posY + 22, nameComment, 20, 1);
+  }
+  //Vẽ các đường giá trị ngang trong biểu đồ
+  for (let i = 0; i <= numY; i++) {
+    drawText(ctx, posX, posY + i * 50, numY - i, 15);
+    drawLine(ctx, posX + 20, posY + i * 50, posX + 20 + numX * 100, posY + i * 50);
+  }
+  //Vẽ các cột giá trị của biểu đồ
+  for (let i = 0; i < numX; i++) {
+    drawText(ctx, posX + 50 + i * 100, posY + numY * 50 + 30, xcont[i], 15);
+    if (yCount[i] == 0) {
+      drawRect(ctx, posX + 20 + i * 100, posY + (numY - yCount[i]) * 50, 50, yCount[i] * 50 -10, color);
+    }
+    else {
+      drawRect(ctx, posX + 20 + i * 100, posY + (numY - yCount[i]) * 50, 50, yCount[i] * 50, color);
+    }
+  }
+}
+//clear biểu đồ
+function clearChart() {
+  const colGraph = document.getElementById("col__chart");
+  const ctx = colGraph.getContext("2d");
+  ctx.clearRect(0, 0, colGraph.width, colGraph.height);
+}
+
+export { drawGraphCol, clearChart }
--- js/index.js
+++ js/index.js
@@ -1,273 +1,104 @@
-let monthListCol = document.getElementById("month__list");
-let yearListCol = document.getElementById("year__list");
-let calendar = document.getElementById("calendar");
-let buttonPreviousYear = document.getElementById("button__previous__year");
-let buttonPreviousMonth = document.getElementById("button__previous__month");
-let buttonNextYear = document.getElementById("button__next__year");
-let buttonNextMonth = document.getElementById("button__next__month");
-let inputBirthDay = document.getElementById("input__birthday")
-let buttonSubmit = document.getElementById("button__submit");
-let inputUsername = document.getElementById("input__username");
-let inputPassword = document.getElementById("input__password");
-let inputEmail = document.getElementById("input__email");
-let errorUsername = document.getElementById("error__username");
-let errorPassword = document.getElementById("error__password");
-let errorEmail = document.getElementById("error__email");
-let errorBirthday = document.getElementById("error__birthday");
-let username,password, email,birthday, dayInput;
-let months = [
-     {
-          name: "Tháng 1",
-          value: 1
-     },
-     {
-          name: "Tháng 2",
-          value: 2
-     },
-     {
-          name: "Tháng 3",
-          value: 3
-     },
-     {
-          name: "Tháng 4",
-          value: 4
-     },
-     {
-          name: "Tháng 5",
-          value: 5
-     },
-     {
-          name: "Tháng 6",
-          value: 6
-     },
-     {
-          name: "Tháng 7",
-          value: 7
-     },
-     {
-          name: "Tháng 8",
-          value: 8
-     },
-     {
-          name: "Tháng 9",
-          value: 9
-     },
-     {
-          name: "Tháng 10",
-          value: 10
-     },
-     {
-          name: "Tháng 11",
-          value: 11
-     },
-     {
-          name: "Tháng 12",
-          value: 11
-     },
+import {drawGraphCol, clearChart} from "./draw.js"
+/* khai báo các biến */
+let color = "aqua";
+let nameChart = $('input[name="name__chart"]').val();
+let nameProject = $('input[name="name__project"]').val();
+let nameComment = $('input[name="name__comment"]').val();
+let maxY = $('input[name="maxy"]').val();
+let maxX = $('input[name="maxx"]').val();
+let xPosition = 150, yPosition = 150;
+let yCount = [2, 0, 3, 4, 4];
 
-]
-let today = new Date();
-let monthSelected = today.getMonth()+1, yearSelected = today.getFullYear();
+/* khởi tạo biểu đồ ban đầu */
+renderValueAndCollumn();
+drawGraphCol(xPosition, yPosition, maxY, maxX, color, nameChart, nameProject, nameComment, yCount);
 
-//Thêm combobox tháng và năm vào table
-function addMonthYearList() {
-     //tạo select month
-     let selectionMonth = document.createElement("select");
-     selectionMonth.setAttribute("id", "select__month");
-     selectionMonth.setAttribute("class","calendar__dropdown");
-     selectionMonth.setAttribute("name", "select__month");
-     selectionMonth.addEventListener('change', function () {
-          monthSelected = selectionMonth.options[selectionMonth.selectedIndex].value;
-          yearSelected = selectionYear.options[selectionYear.selectedIndex].value;
-          getDay(monthSelected, yearSelected)
-     })
-     //tạo select year
-     let selectionYear = document.createElement("select");
-     selectionYear.setAttribute("id", "select__year");
-     selectionYear.setAttribute("class","calendar__dropdown");
-     selectionYear.setAttribute("name", "select__year");
-     selectionYear.addEventListener('change', function () {
-          monthSelected = selectionMonth.options[selectionMonth.selectedIndex].value;
-          yearSelected = selectionYear.options[selectionYear.selectedIndex].value;
-          getDay(monthSelected,yearSelected)
-     })
-     //add option vào select
-     //add month
-     months.forEach(month => {
-          let newMonth = document.createElement("option");
-          newMonth.text = month.name;
-          newMonth.setAttribute("value", month.value);
-          selectionMonth.appendChild(newMonth);
-     });
-     //add year
-     for (let i = 2000; i <= 2050; i++) {
-          let newYear = document.createElement("option");
-          newYear.text = i;
-          newYear.setAttribute("value", i);
-          selectionYear.appendChild(newYear);
-     }
-     //add select vào table
-     monthListCol.appendChild(selectionMonth);
-     yearListCol.appendChild(selectionYear);
+/* Render ra giá trị các cột */
+function renderValueAndCollumn(){
+  let sel = document.getElementsByClassName("controller__input__col")[0];
+  let value ="";
+  maxX = $('input[name="maxx"]').val();
+  for(let i = 1; i <= 12; i++){
+    value+= `<div class="controller__input__col__item">
+              <label for="">Cột ${i}:</label>
+              <input name="colnum" type="number" min="0" max="12" value="${yCount[i-1]}">
+            </div>`;
+    sel.innerHTML = value;
+  }
 }
-//Kiểm tra năm nhuận
-function isLeap(year) {
-     if ((year % 4) || ((year % 100 === 0) && (year % 400))) return 0;
-     else return 1;
+/* Update giá trị các cột */
+function updateValueCollumn(){
+  maxX =  $('input[name="maxx"]').val();
+  for(let i = 0; i < 12; i++){
+    let va = parseInt(document.getElementsByName("colnum")[i].value);
+    if(va > maxY){
+      $('input[name="maxy"]').val() = va;
+      maxY = va;
+      clearChart();
+      drawGraphCol(xPosition, yPosition, maxY, maxX, color, nameChart, nameProject, nameComment,yCount);
+    }
+    yCount[i] = parseInt(document.getElementsByName("colnum")[i].value);
+  }
 }
-function getDay(monthSelected, yearSelected) {
-     let lengthTable = calendar.rows.length
-     for(let index = 2 ; index < lengthTable ; index++){
-          calendar.deleteRow(2);
-     }
-     let check = today.getMonth()+1 == monthSelected && today.getFullYear() == yearSelected ? true : false;
-     let date = new Date(yearSelected, monthSelected - 1, 1);
-     let dayInTime = monthSelected == 2 ? (28 + isLeap(yearSelected)) : 31 - (monthSelected - 1) % 7 % 2;
-     let count = 1;
-     for (let i = 1; i <= 6; i++) {
-          let tr = document.createElement("tr");
-          tr.setAttribute("class","calendar__content")
-          // let row = calendar.insertRow(calendar.rows.length);
-          if (i == 1 & count <= dayInTime) {
-               for (let y = 0; y < date.getDay(); y++) {
-                    let td = document.createElement("td");
-                    tr.appendChild(td);
-                    // let cell = row.insertCell(y)
-                    // cell.innerHTML = "";
-               }
-               for (let j = date.getDay(); j <= 6; j++) {
-                    if (count <= dayInTime) {
-                         let td = document.createElement("td");
-                         if(check && count == today.getDate()){
-                              td.setAttribute("background-color","aqua");
-                         }
-                         td.addEventListener("click",function(){
-                              dayInput = td.innerText;
-                              inputBirthDay.value=dayInput + "/" + monthSelected + "/" + yearSelected;
-                         })
-                         td.innerHTML = count;
-                         tr.appendChild(td);
-                         // let cell = row.insertCell(j)
-                         // cell.innerHTML = count;
-                         count++;
-                    }
-               }
-          }
-          else if(count<= dayInTime){
-               for (let j = 0; j <= 6; j++) {
-                    if (count <= dayInTime) {
-                         let td = document.createElement("td");
-                         if(check && count == today.getDate()){
-                              td.style.backgroundColor = "aqua";
-                         }
-                         td.addEventListener("click",function(){
-                              dayInput = td.innerText;
-                              inputBirthDay.value=monthSelected  + "/" +dayInput  + "/" + yearSelected;
-                         })
-                         td.innerHTML = count;
-                         tr.appendChild(td);
-                         // let cell = row.insertCell(j)
-                         // cell.innerHTML = count;
-                         count++;
-                    }
-               }
-          }
-          calendar.appendChild(tr);
-     }
-}
-function applyEventForButton() {
-     let yearSelection = document.getElementById("select__year");
-     let monthSelection = document.getElementById("select__month");
-     yearSelection.value = yearSelected;
-     monthSelection.value = monthSelected;
-     buttonPreviousYear.addEventListener('click', function(){
-          if(yearSelection.selectedIndex != 0){
-               yearSelection.selectedIndex -= 1;
-               monthSelected = monthSelection.childNodes[monthSelection.selectedIndex].value;
-               yearSelected = yearSelection.childNodes[yearSelection.selectedIndex].value;              
-               getDay(monthSelected,yearSelected);
-          }
-     })
-     buttonPreviousMonth.addEventListener('click', function(){
-          if(monthSelection.selectedIndex != 0){
-               monthSelection.selectedIndex -= 1;
-               monthSelected = monthSelection.childNodes[monthSelection.selectedIndex].value;
-               yearSelected = yearSelection.childNodes[yearSelection.selectedIndex].value;              
-               getDay(monthSelected,yearSelected);
-          }
-     })
-     buttonNextYear.addEventListener('click', function(){
-          if(yearSelection.selectedIndex != yearSelection.options.length -1){
-               yearSelection.selectedIndex += 1;
-               monthSelected = monthSelection.childNodes[monthSelection.selectedIndex].value;
-               yearSelected = yearSelection.childNodes[yearSelection.selectedIndex].value;              
-               getDay(monthSelected,yearSelected);
-          }
-     })
-     buttonNextMonth.addEventListener('click', function(){
-          if(monthSelection.selectedIndex != monthSelection.options.length -1){
-               monthSelection.selectedIndex += 1;
-               monthSelected = monthSelection.childNodes[monthSelection.selectedIndex].value;
-               yearSelected = yearSelection.childNodes[yearSelection.selectedIndex].value;              
-               getDay(monthSelected,yearSelected);
-          }
-     })
-     buttonSubmit.addEventListener('click',function(){
-          if(validateInput()){
-               username = inputUsername.value;
-               password = inputPassword.value;
-               email = inputEmail.value;
-               birthday = inputBirthDay.value;
-               // config.addUser(username,password,email,birthday);
-          }
-     })
-}
-function applyEventForInputDate(){
-     inputBirthDay.addEventListener("click",function(){
-          calendar.style.display = "inline";
-     })
-     // inputBirthDay.addEventListener("focusout",function(){
-     //      calendar.style.display = "none";
-     // })
-}
-//validate các input
-function validateInput(){
-     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-     username = inputUsername.value;
-     password = inputPassword.value;
-     email = inputEmail.value;
-     birthday = inputBirthDay.value;
-     errorUsername.style.display = username == "" || username.length < 8 ? "inherit" : "none";
-     errorPassword.style.display = password == "" || password.length < 8 ? "inherit" : "none";
-     errorEmail.style.display = email == "" || email.length < 8 || !re.test(String(email).toLowerCase()) ? "inherit" : "none";
-     let convertDate = new Date(birthday);
-     errorBirthday.style.display = birthday == "" ||  today.getTime() - convertDate.getTime() < 0 ? "inherit" : "none";
-     // console.log(today.getTime() - convertDate.getTime())
-     if(errorUsername.style.display == "inherit" || errorPassword.style.display == "inherit" || errorEmail.style.display == "inherit" ||errorBirthday.style.display=="inherit"){
-          console.log(false);
-          return false;
-     }
-     else{
-          console.log(true);
-          return true;
-     }
-     // if(username == "" ||password == "" ||email == "" || birthDay.value == ""){
-     //      console.log(false);        
-     // }
-     // else{
-     //      if(username.length < 8 || password.length < 8 || email.length < 8){
-     //           console.log(false);
-
-     //      }
-          
-     // };
-
-}
+/* Jquery*/
+$(document).ready(function(){
+  $('input[name="colnum"]').change(function () { 
+    updateValueCollumn();
+    clearChart();
+    drawGraphCol(xPosition, yPosition, maxY, maxX, color, nameChart, nameProject, nameComment,yCount);
+  });
+  
+  $('input[name="name__chart"]').keyup(function () { 
+    // nameChart = document.getElementsByName("name__chart")[0].value;
+    nameChart = $('input[name="name__chart"]').val();
+    clearChart();
+    drawGraphCol(xPosition, yPosition, maxY, maxX, color, nameChart, nameProject, nameComment,yCount);
+  });
+  
+  $('input[name="name__project"]').keyup(function () { 
+    nameProject = $('input[name="name__project"]').val();
+    clearChart();
+    drawGraphCol(xPosition, yPosition, maxY, maxX, color, nameChart, nameProject, nameComment,yCount);
+  });
+  
+  $('input[name="name__comment"]').keyup(function () { 
+    nameComment =  $('input[name="name__comment"]').val();
+    clearChart();
+    drawGraphCol(xPosition, yPosition, maxY, maxX, color, nameChart, nameProject, nameComment,yCount);
+  });
+  $('input[name="maxy"]').change(function () { 
+    maxY = $('input[name="maxy"]').val();
+    let check = false;
+    console.log(maxY)
+    yCount.forEach(index => {
+      if(index >maxY){
+        check=true;
+      }
+    });
+    if(!check){
+      clearChart();
+      drawGraphCol(xPosition, yPosition, maxY, maxX, color, nameChart, nameProject, nameComment,yCount);
+    }
+  });
+  $('input[name="maxx"]').change(function () { 
+    maxX = $('input[name="maxx"]').val();
+    clearChart();
+    drawGraphCol(xPosition, yPosition, maxY, maxX, color, nameChart, nameProject, nameComment,yCount);
+  });
+  
+  $(".controller__color__item").click(function () { 
+    color = $(this).css( "background-color" );
+    clearChart();
+    drawGraphCol(xPosition, yPosition, maxY, maxX, color, nameChart, nameProject, nameComment,yCount);
+  });
+  
+  $("#dropdown__icon").click(function () { 
+    let display = $(".controller__input__col").css("display"); 
+    if(display == "none")
+      $(".controller__input__col").css("display", "block");
+    else
+      $(".controller__input__col").css("display", "none");
+  });
+})
 
 
-
-calendar.style.display = "none";
-addMonthYearList();
-getDay(monthSelected,yearSelected)
-applyEventForButton();
-applyEventForInputDate();
